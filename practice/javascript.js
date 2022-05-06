0a263253eca7f03ac19f74c6d4fc345a644b1ed1
marco <migliavacca.marco08@gmail.com>
2022-05-06 11:47:03 +0200

    Do exercise prompting for correct username and password from js.info

--- practice/javascript.js
+++ practice/javascript.js
@@ -1,7 +1,21 @@
-let admin, name;
+const correctUserName = "Admin"
+const correctPassoword = "Master"
 
-name = "John";
 
-admin = name;
+let userName = prompt("Who's there?")
 
-console.log(admin);
+if (userName == "" || userName == null) {
+   alert("Cancelled")
+}  else if (userName != "Admin") {
+   alert("No bro")
+}  else if (userName == correctUserName) {
+   let password = prompt("OK, password?")
+   if (password == "" || password == null) {
+      alert("Cancelled")
+   } else if (password != "Master") {
+      alert("Bro password sbagliata")
+   }
+   else if (password == correctPassoword){
+      alert ("Welcome")
+   }
+}
